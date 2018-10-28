562ba9abdd4767f353a82f4ad96ed73fe6cc0e92
JeffDropsIT <mathonsiphumzile@gmail.com>
2018-10-28 15:14:30 +0200

    routes mod

--- controllers/greetings.js
+++ controllers/greetings.js
@@ -1,6 +1,6 @@
 
 const greeting = function(ctx){
-    ctx.body = "Welcome to node js, I love Koajs";
+    ctx.body = "Welcome to ZipTown, a product of DevDesign Pty Ltd.";
 }
 module.exports = {
     greeting
